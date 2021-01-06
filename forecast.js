9cfac1d9cf9558123f33c223ca8762e98f19d25b
push57 <pushpanathanmurali@gmail.com>
2021-01-06 13:54:24 +0530

    Add files via upload

---
+++ forecast.js
@@ -0,0 +1,42 @@
+$(document).ready(function(){
+	$("#submitForecast").click(function(){
+		return getForecast();
+	});
+});
+
+function getForecast(){
+	var city = $("#city").val();
+	var days = $("#days").val();
+
+	if(city != '' && days != ''){
+
+		$.ajax({
+			url:'http://api.openweathermap.org/data/2.5/forecast?q=' + city + "&units=metric" + "&cnt=" + days + 
+			"&APPID=7b90075f4201218c835d0dd017439a75",
+			type:"GET",
+			dataType:"jsonp",
+			success:function(data){
+				
+					var table = '';
+
+					var header='<h2 style="font-weight:bold; font-size:30px; margin-top:20px;">Weather forecast for' + data.city.name +' ,' + data.city.country + '</h2>'
+
+					for (var i = 0; i < data.list.length; i++){
+						table +="<tr>";
+
+						table +="<td><img src='http://openweathermap.org/img/w/" + data.list[i].weather[0].icon + ".png'</td>";
+						table +="<td>" + data.list[i].weather[0].main + "</td>";
+						table +="<td>" + data.list[i].weather[0].description + "</td>";
+                        table +="</tr>";
+					}
+				
+				$("#forecastWeather").html(table);
+				$("#header").html(header);
+				$("#city").val('');
+				$("#days").val('');
+			}
+		});
+	}else{
+		$("#error").html("<div class='alert alert-danger' id='errorCity'><a href='#' class='close' data-dismiss='alert' aria-label='close'>&times;</a>City field cannot be empty</div>")
+	}
+}
